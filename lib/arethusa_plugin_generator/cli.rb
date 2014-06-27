43f8f3b5d597185fdb9794226a5a8f6d5566985b
LFDM <1986gh@gmail.com>
2014-06-27 10:31:17 +0200

    Add new method

--- lib/arethusa_plugin_generator/cli.rb
+++ lib/arethusa_plugin_generator/cli.rb
@@ -3,5 +3,23 @@ require 'thor'
 module ArethusaPluginGenerator
   class CLI < Thor
     include Thor::Actions
+
+    attr_reader :namespace, :name
+
+    desc 'new', 'Creates a new Arethusa plugin skeleton'
+    method_option :namespace, aliases: '-n',
+      desc: 'Namespace of the new plugin'
+    def new(name)
+      @name = name
+      @namespace = options[:namespace] || 'arethusa'
+
+      say_status(:success, "Created #{namespaced_name}")
+    end
+
+    no_commands do
+      def namespaced_name
+        [namespace, name].compact.join('.')
+      end
+    end
   end
 end
