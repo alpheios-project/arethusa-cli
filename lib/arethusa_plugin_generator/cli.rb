218b0a3c8c350e2610d39e5d10e02f5bc8e56afc
LFDM <1986gh@gmail.com>
2014-06-27 10:33:52 +0200

    Add directory creation to #new

--- lib/arethusa_plugin_generator/cli.rb
+++ lib/arethusa_plugin_generator/cli.rb
@@ -13,12 +13,36 @@ module ArethusaPluginGenerator
       @name = name
       @namespace = options[:namespace] || 'arethusa'
 
+      create_directories
+
+      puts
       say_status(:success, "Created #{namespaced_name}")
     end
 
     no_commands do
       def namespaced_name
         [namespace, name].compact.join('.')
+      end
+
+      DIRECTORIES = %w{ plugin_dir template_dir }
+      def create_directories
+        DIRECTORIES.each { |dir| empty_directory(send(dir)) }
+      end
+
+      def plugin_dir
+        File.join(js_dir, namespaced_name)
+      end
+
+      def template_dir
+        File.join(temp_dir, namespaced_name)
+      end
+
+      def js_dir
+        File.join(destination_root, 'app/js')
+      end
+
+      def temp_dir
+        File.join(destination_root, 'app/templates')
       end
     end
   end
