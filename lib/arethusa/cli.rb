0d298e948b504251ca55f8d734900f677d856daf
LFDM <1986gh@gmail.com>
2014-06-29 17:10:01 +0200

    Add arethusa deploy command

--- lib/arethusa/cli.rb
+++ lib/arethusa/cli.rb
@@ -12,14 +12,62 @@ module Arethusa
     register(Generator, Generator.namespace, "#{Generator.namespace} [ACTION]", 'Generates Arethusa files. Call "arethusa generate" to learn more')
 
     desc 'build', 'Creates a tar archive to be used for deployment'
+    method_option :minify, aliases: '-m', type: :boolean, default: true,
+      desc: 'Minifies Arethusa before building'
     def build
+      minify if options[:minify]
       empty_directory('deployment')
       @filename = "#{tar_name}#{ending}"
       create_tgz
       say_status(:built, archive_path)
     end
 
+    desc 'deploy ADDRESS DIRECTORY', 'Deploys an Arethusa archive through ssh'
+    method_option :options, aliases: '-o',
+      desc: 'Options to pass to the ssh command'
+    method_option :file, aliases: '-f',
+      desc: 'Archive file to use - builds a new one by default'
+    method_option :minify, aliases: '-m', type: :boolean, default: true,
+      desc: 'Minifies Arethusa before building'
+    def deploy(address, directory)
+      @address = address
+      @directory = directory
+      @ssh_options = options[:options]
+      @archive = options[:file]
+
+      minify if options[:minify] &! @archive
+      execute
+      say_status(:deployed, "at #{@address} - #{@directory}")
+    end
+
     no_commands do
+      def minify
+        `grunt minify`
+        say_status(:success, 'minified Arethusa')
+      end
+
+      # For deploy command
+      def execute
+        `#{archive_to_use} | #{ssh} #{decompress}`
+      end
+
+      def compress
+        "tar -zc #{folders_to_deploy.join(' ')}"
+      end
+
+      def archive_to_use
+        @archive ? "cat #{@archive}" : compress
+      end
+
+      def ssh
+        "ssh #{@ssh_options} #{@address}"
+      end
+
+      def decompress
+        "tar -zxC #{@directory}"
+      end
+
+      # For build command
       def create_tgz
         `tar -zcf #{archive_path} #{folders_to_deploy.join(' ')}`
       end
