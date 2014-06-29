11cd9edb8c33c8c9c9b3d537ced123f4ddbbb3f0
LFDM <1986gh@gmail.com>
2014-06-29 16:19:30 +0200

    Add arethusa build command

--- lib/arethusa/cli.rb
+++ lib/arethusa/cli.rb
@@ -7,6 +7,54 @@ module Arethusa
 
     require 'arethusa/cli/generator'
 
+    include Thor::Actions
+
     register(Generator, Generator.namespace, "#{Generator.namespace} [ACTION]", 'Generates Arethusa files. Call "arethusa generate" to learn more')
+
+    desc 'build', 'Creates a tar archive to be used for deployment'
+    def build
+      empty_directory('deployment')
+      @filename = "#{tar_name}#{ending}"
+      create_tgz
+      say_status(:built, archive_path)
+    end
+
+    no_commands do
+      def create_tgz
+        `tar -zcf #{archive_path} #{folders_to_deploy.join(' ')}`
+      end
+
+      def folders_to_deploy
+        %w{ app bower_components dist vendor }
+      end
+
+      def archive_path
+        "deployment/#{@filename}"
+      end
+
+      def tar_name
+        [tar_namespace, timestamp, git_branch, commit_sha].join('-')
+      end
+
+      def ending
+        '.tgz'
+      end
+
+      def tar_namespace
+        'arethusa'
+      end
+
+      def git_branch
+        `git rev-parse --abbrev-ref HEAD`.strip
+      end
+
+      def timestamp
+        Time.now.to_i
+      end
+
+      def commit_sha
+        `git rev-parse --short HEAD`.strip
+      end
+    end
   end
 end
