56791e6783a0370d35131ef10733cf72ecf78bb3
LFDM <1986gh@gmail.com>
2014-08-17 16:23:39 +0200

    Transform relation configs

--- lib/arethusa/cli/transformer.rb
+++ lib/arethusa/cli/transformer.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
+require 'json'
 
 class Arethusa::CLI
   class Transformer < Subcommand
@@ -6,10 +7,59 @@ class Arethusa::CLI
 
     desc 'relation [FILE]', 'Transforms relation configurations'
     def relation(file)
+      @doc = Nokogiri::XML(open(file))
+      @conf = Relations.new
+
+      parse('relation', :add_label)
+      parse('subrelation', :add_suffix)
+
+      puts @conf.to_json
     end
 
     no_commands do
+      def parse(type, method)
+        @doc.xpath("//xmlns:table[@type = '#{type}']/xmlns:entry").each do |entry|
+          short = entry.xpath('./xmlns:tb').text
+          next if short == '-' || short.empty?
+          long  = entry.xpath('./xmlns:menu').text
+          @conf.send(method, stripped(short), stripped(long))
+        end
+      end
 
+      def stripped(str)
+        str.sub(/^_/, '')
+      end
+
+      class Relations
+        def initialize
+          @labels = {}
+          @suffixes = {}
+        end
+
+        def add_label(short, long)
+          @labels[short] = Relation.new(short, long)
+        end
+
+        def add_suffix(short, long)
+          @suffixes[short] = Relation.new(short, long)
+        end
+
+        def to_json(options = {})
+          hsh = { relations: { labels: @labels, suffixes: @suffixes } }
+          hsh.to_json(options)
+        end
+      end
+
+      class Relation
+        def initialize(short, long)
+          @short = short
+          @long  = long
+        end
+
+        def to_json(options = {})
+          { short: @short, long: @long }.to_json(options)
+        end
+      end
     end
   end
 end
