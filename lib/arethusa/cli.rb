17c546d6b3664928370b3ad3955d94a469970578
LFDM <1986gh@gmail.com>
2014-10-29 19:04:45 +0100

    Fix minification check
    Used the wrong type of system call - we need the return status to know
    if our processes really worked out ok.

--- lib/arethusa/cli.rb
+++ lib/arethusa/cli.rb
@@ -147,7 +147,7 @@ EOF
       end
 
       def minify
-        if `bower install && grunt clean minify:all`
+        if system('bower install && grunt clean minify:all')
           say_status(:success, 'minified Arethusa')
         else
           say_status(:error, 'minification failed')
