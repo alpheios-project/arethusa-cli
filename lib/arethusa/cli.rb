84990273be7f6ceacb2e1bb5c1e591982bd3f758
LFDM <1986gh@gmail.com>
2014-11-03 23:30:44 +0100

    Third fix for ssh and passing commands...

--- lib/arethusa/cli.rb
+++ lib/arethusa/cli.rb
@@ -185,7 +185,7 @@ EOF
       end
 
       def use_deployment_helper
-        `#{ssh} "bash -l -c 'depl-help-deploy arethusa #{@directory} #{@environment}'"`
+        `#{ssh} -t "bash -l -c 'depl-help-deploy arethusa #{@directory} #{@environment}'"`
       end
 
       def compress
